1f575b660d788bd8d8777ab7125a14323ebdbae1
Wanderson Pinto <wanpdsantos@gmail.com>
2021-03-15 08:24:08 -0300

    TopBar Responsive Drawer

--- components/Layout/TopBar.js
+++ components/Layout/TopBar.js
@@ -1,11 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Hidden from '@material-ui/core/Hidden';
 import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer'
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import Link from 'next/link';
 
 const useStyles = makeStyles((theme) => ({
@@ -40,8 +46,16 @@ const useStyles = makeStyles((theme) => ({
 
 function TopBar () {
     const classes = useStyles();
-    return(
 
+    const [state, setState] = React.useState(false);
+
+    const toogleDrawer = (open) => (event ) => {
+        setState(open);
+    };
+        
+    return(
+    
+    <>
         <AppBar className={classes.root} color="default">
 
             <Toolbar className={classes.toolbar}>
@@ -49,7 +63,15 @@ function TopBar () {
                 <Hidden mdUp>
 
                     <Box alignItems='center'>
-                        <MenuIcon/>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={toogleDrawer(true)}
+                            edge="start"
+                            
+                        >
+                            <MenuIcon />
+                        </IconButton>
                     </Box>
 
                     <Box alignItems='center'>
@@ -92,12 +114,37 @@ function TopBar () {
 
                 </Hidden>
 
-
-
-
             </Toolbar>
 
         </AppBar>
+
+        <SwipeableDrawer
+            anchor="top"
+            open={state}
+            onClose={toogleDrawer(false)}
+        >
+
+            <List>
+                <ListItem button key='Membros'>
+                    <Link href="/Membros" passHref>
+                        <ListItemText primary='Membros' />
+                    </Link>
+                </ListItem>
+                <ListItem button key='Sobre'>
+                    <Link href="/Sobre" passHref>
+                        <ListItemText primary='Sobre' />
+                    </Link>
+                </ListItem>
+                <ListItem button key='Loja'>
+                    <Link href="/Loja" passHref>
+                        <ListItemText primary='Loja' />
+                    </Link>
+                </ListItem>
+            </List>
+
+        </SwipeableDrawer>
+    
+    </>
     );
 
 }
